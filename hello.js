ce2512ca3e85bce6996cb66295060d1f76a753de
Jelena Planic <124892549+JelenaPlanic@users.noreply.github.com>
2026-02-19 19:51:49 +0100

    ticket-22 make changes (one commit) (#2)
    ticket-22 make changes (resolve rebase conflict)

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 console.log("Hello from the TICKET-11 Branch");
+=======
+console.log("Hello from the TICKET-22Branch");
+console.log("Hello again from ticket -22 ");
+
+>>>>>>> d970c40 (ticket-22 make changes (one commit))
